c182f3e5350b6626a4e6e187a89c59a75e447ab9
mathiasomar <ommykmathy@gmail.com>
2025-08-13 12:15:32 +0300

    enum type

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import User from "./components/Person";
+import { Countries } from "./components/Person";
 import { UserContextProvider } from "./context/UserContextProvider";
 
 function App() {
   return (
     <UserContextProvider>
-      <User name="Omar" age={25} isMarried={false} />
-      <User name="John" age={26} isMarried={true} />
+      <User name="Omar" age={25} isMarried={false} country={Countries.Brazil} />
+      <User name="John" age={26} isMarried={true} country={Countries.France} />
     </UserContextProvider>
   );
 }
--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,20 +1,30 @@
-import { useContext } from "react";
-import { UserContext } from "../context/UserContextProvider";
+// import { useContext } from "react";
+// import { UserContext } from "../context/UserContextProvider";
 
 type PersonProps = {
   name: string;
   age: number;
   isMarried: boolean;
+  country: Countries;
 };
 
-const User = ({ name, age, isMarried }: PersonProps) => {
-  const {} = useContext(UserContext);
+// enum
+export enum Countries {
+  Brazil = "Brazil",
+  France = "France",
+  India = "India",
+  UnitedStates = "United States",
+}
+
+const User = ({ name, age, isMarried, country }: PersonProps) => {
+  //   const {} = useContext(UserContext);
   return (
     <div>
       <>
         <p>Name: {name}</p>
         <p>Age: {age}</p>
         <p>Married: {isMarried ? "Yes" : "No"}</p>
+        <p>Country: {country}</p>
       </>
     </div>
   );
